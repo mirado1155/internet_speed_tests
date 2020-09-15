8009f17735d925940d0c61bb178556121f432235
Robert Adams <rjadams1991@gmail.com>
2020-09-15 17:00:37 -0500

    changed some values to constants

--- speed_test.py
+++ speed_test.py
@@ -28,16 +28,19 @@ def process_csv():
 
 # This function executes a command which tests internet speed and outputs to a csv file. Set on timer.
 def set_continuous_tests():
-    for test in range(6):
+    NUM_TESTS = 6
+    for test in range(NUM_TESTS):
         curr_time = datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")
         with open("speed_test_results.csv", "a") as write_file:
             writer = csv.writer(write_file)
             writer.writerow(["Time: {}".format(curr_time)])
         subprocess.call(["speedtest.exe", "--format=csv", ">>", "speed_test_results.csv"], shell=True)
         print("Test completed: {}".format(curr_time))
-        time.sleep(900)
+
+        if (test != NUM_TESTS):
+            time.sleep(600)
         
 
 if __name__ == "__main__":
-    set_continuous_tests()
+    # set_continuous_tests()
     process_csv()
