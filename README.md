99fe88c656467ed7ce7be90ca8a937b73428cf8e
Robert Adams <rjadams1991@gmail.com>
2020-09-24 12:11:17 -0500

    rerouted all output to files for compatability with pyw.exe. Now this application can be run in the background! Updated README

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # internet_speed_tests
 
-This application will test your internet down/up speed at every quarter-hour and output it to a .csv file. 
+This application will test your internet down/up speed at every quarter-hour and output it to a .csv file.
+
+If all goes well, this can be run with pyw.exe to run in background!
 
 User must download speedtest cli for python https://pypi.org/project/speedtest-cli/
--- speed_test.py
+++ speed_test.py
@@ -4,33 +4,43 @@ import speedtest
 import csv
 
 
-# This function will run the test_speed() function at every quarter of the hour precisely.
-def timed_sample_output():
+STAUTS_OUTPUT_FILE = 'output/status.txt'
+DATA_FILE = 'output/output_file.csv'
 
+
+'''Outputs current status to txt file'''
+def write_status(output_file, output):
+    with open(output_file, 'a') as writer:
+                    writer.write(output)
+
+
+'''Run the test_speed() function at every quarter of the hour precisely.'''
+def timed_sample_output():
     SECS_BETWEEN_LOOPS = 120 # How long between each "not time yet" message
     times = ['00', '15', '30', '45'] # List of quarter-hours
 
-    # Loops to continually test current time against quarter-hours
+    
     counter = 1 # keeps track of how many loop loops have been looped
-    while(True):
+    while(True): # Loop to continually test current time against quarter-hours
         current_time = datetime.now().strftime('%H:%M')
+        is_time_message = "It is time {}\n".format(current_time)
+        not_time_message = "It is not time yet... {}\n".format(current_time)
         for quarter in times:
             if (current_time[3:5] == quarter):
-                print("It is time {}".format(current_time))
+                write_status(STAUTS_OUTPUT_FILE, is_time_message)
                 test_speed(current_time)
-                # This ensures a test isn't called twice for the same quarter-hour
-                time.sleep(60)
+                time.sleep(60) # Ensures a test isn't called twice for the same quarter-hour
                 break
             elif (quarter == times[3]):
-                if (counter == SECS_BETWEEN_LOOPS): # basically for every however many seconds, a "not time yet" message displays
-                    print("It is not time yet... {}".format(current_time))
+                if (counter >= SECS_BETWEEN_LOOPS): # for every however many seconds, a "not time yet" message displays
+                    write_status(STAUTS_OUTPUT_FILE, not_time_message)
                     counter = 0
         counter += 1
         time.sleep(1)
 
 
-# Takes a sample of internet connection Down/Up speeds and puts the results in a dictionary
-# Outputs the results of each test onto console
+'''Takes a sample of internet connection Down/Up speeds and puts the results in a dictionary
+    Outputs the results of each test onto console'''
 def test_speed(curr_time):
 
     SPEED_DIVISOR = 1000000 # for converting bits per second into Mbps
@@ -45,20 +55,19 @@ def test_speed(curr_time):
         "Download":round((download_time / SPEED_DIVISOR), 2),
         "Upload":round((upload_time / SPEED_DIVISOR), 2)
     }
-    print("Trial for time: {} completed".format(curr_time))
-    print(output)
+    write_status(STAUTS_OUTPUT_FILE, "Trial for time: {} completed\n{}\n".format(curr_time, output))
     write_to_file(output)
 
 
-# Takes a test conducted by test_speed() and sticks it in a csv file.
+'''Takes a test conducted by test_speed() and sticks it in a csv file.'''
 def write_to_file(output):
-    with open('output_file.csv', 'a') as write_file:
+    with open(DATA_FILE, 'a') as write_file:
         columns = ['Date', 'Time', 'Download', 'Upload']
         writer = csv.DictWriter(write_file, fieldnames=columns)
         # writer.writeheader() -- Include this if you want a header with the data
         writer.writerow(output)
 
 
-# Run the program!
+'''Run the program!'''
 if __name__ == "__main__":
     timed_sample_output()
